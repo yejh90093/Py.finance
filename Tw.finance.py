293e73eef7db6a917ba44a257c4d288f1f104d9f
andy.chen <yejh90093@gmail.com>
2020-05-19 21:28:45 +0800

    Add code to files
    Add code to files

---
+++ Tw.finance.py
@@ -0,0 +1,85 @@
+import numpy
+import requests
+import datetime
+import time
+import math
+import pandas as pd
+import functions
+import numpy as np
+from tqdm import tqdm
+
+pbar = tqdm(total=972)
+
+
+now = datetime.datetime.now()
+
+dayStart = str(int(time.time()))
+dayEnd = str(int(time.time()) - 8640000)
+monthEnd = str(int(time.time()) - 686400000)
+
+all = functions.readAll()
+
+resultDic = {}
+idArr = []
+tempArr = []
+nameArr = []
+dayWilliamsRArr = []
+dayRSIArr = []
+monthRSIArr = []
+process = 0
+
+#print(all.keys())
+
+for value in all.values:
+    # value[0] = "2887F"
+    # print (process, value[0], value[1])
+
+    tempArr.append(value[0])
+    nameArr.append(value[1])
+
+    responseDay = functions.getFinanceData(value[0], dayStart, dayEnd, "1d")
+    dataArrayDay = functions.dataTextToArray(responseDay.text)
+
+    arrWilliamsR = functions.arrayWilliamsR(dataArrayDay, 50)
+    arrRSI = functions.arrayRSI(dataArrayDay, 60)
+
+    dayWilliamsR = arrWilliamsR[len(arrWilliamsR) - 1][9]
+    dayRSI = arrRSI[len(arrRSI) - 1][7]
+
+    dayWilliamsRArr.append(dayWilliamsR)
+    dayRSIArr.append(dayRSI)
+
+    responseMonth = functions.getFinanceData(value[0], dayStart, monthEnd, "1mo")
+    # responseMonth = functions.getFinanceData('2330', dayStart, monthEnd, "1mo")
+    dataArrayMonth = functions.dataTextToArray(responseMonth.text)
+    dataArrayMonth = np.delete(dataArrayMonth, len(dataArrayMonth) - 2, axis=0)
+
+    arrRSIMonth = functions.arrayRSI(dataArrayMonth, 4)
+    if len(arrRSIMonth) <= 1:
+        monthRSI = None
+    else:
+        monthRSI = arrRSIMonth[len(arrRSIMonth) - 1][7]
+
+    monthRSIArr.append(monthRSI)
+
+    process = process + 1
+    pbar.update(1)
+
+#    if process > 10:
+#        break
+
+
+resultDic['monthRSI'] = monthRSIArr
+resultDic['dayRSI'] = dayRSIArr
+resultDic['dayWilliamsR'] = dayWilliamsRArr
+resultDic[all.keys()[1]] = nameArr
+resultDic[all.keys()[0]] = tempArr
+
+resultDF = pd.DataFrame(resultDic)
+
+pbar.close()
+print (resultDF)
+
+
+#for dfValue in resultDF.values:
+#    print (dfValue[1])
---
+++ functions.py
@@ -0,0 +1,145 @@
+import datetime
+import numpy
+import requests
+from io import StringIO
+import pandas as pd
+import numpy as np
+
+
+def readAll():
+    currentDate = datetime.datetime.utcnow()
+    condition = True
+    while condition:
+        # loop body here
+        currentDate = currentDate - datetime.timedelta(days=1)
+        currentDateStr = currentDate.strftime("%Y%m%d")
+        r = requests.post(
+            'http://www.twse.com.tw/exchangeReport/MI_INDEX?response=csv&date=' + currentDateStr + '&type=ALL')
+        if r.text != "":
+            condition = False
+
+    str_list = []
+    for i in r.text.split('\n'):
+        if len(i.split('",')) == 17 and i[0] != '=':
+            i = i.strip(",\r\n")
+            str_list.append(i)
+
+    df = pd.read_csv(StringIO("\n".join(str_list)))
+    pd.set_option('display.max_rows', None)
+    return df
+
+
+# Valid intervals: [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo]
+def getFinanceData(id, start, end, interval):
+    site = "https://query1.finance.yahoo.com/v7/finance/download/" + id + ".TW?" \
+                                                                          "period1=" + end + \
+           "&period2=" + start + \
+           "&interval=" + interval + \
+           "&events=history" \
+           "&crumb=hP2rOschxO0"
+    response = requests.get(site)
+    #print(site)
+    return response
+
+
+def dataTextToArray(text):
+    df = pd.read_csv(StringIO(text))
+    return df.values
+
+
+def arrayWilliamsR(dataArray, nDay):
+    arrWilliamsR = []
+    NH = []
+    NL = []
+    n = 0
+    nDay = nDay - 1
+
+    for day in dataArray:
+
+        if n >= nDay:
+            NH = []
+            NL = []
+            for i in range(n, n - nDay, -1):
+                NH.append(dataArray[i][2])
+                NL.append(dataArray[i][3])
+
+        if len(NH) == 0:
+            B = dataArray[n]
+
+            A = numpy.array([None, None, None])
+            B = numpy.append(B, A)
+        else:
+            B = dataArray[n]
+            W = (max(NH) - dataArray[n][4]) / (max(NH) - min(NL)) * 100
+
+            A = numpy.array([max(NH), min(NL), W])
+
+            B = numpy.append(B, A)
+
+        # print (n, dataArray[n][0], A)
+        arrWilliamsR.append(B)
+        n = n + 1
+
+    return arrWilliamsR
+
+
+def arrayRSI(dataArray, nDay):
+    arrRSI = []
+    n = 0
+    nDay = nDay - 1
+
+    lastPlus = None
+    lastMinus = None
+
+    for day in dataArray:
+        sumPlus = 0
+        sumMinus = 0
+
+        if n >= nDay:
+
+            if n == nDay:
+                for i in range(n, n - nDay, -1):
+                    dayDiff = dataArray[i][4] - dataArray[i - 1][4]
+                    if numpy.math.isnan(dayDiff): dayDiff = 0
+
+                    if dayDiff < 0:
+                        sumMinus = sumMinus + abs(dayDiff)
+                    else:
+                        sumPlus = sumPlus + abs(dayDiff)
+            else:
+                dayDiff = dataArray[n][4] - dataArray[n - 1][4]
+                if numpy.math.isnan(dayDiff): dayDiff = 0
+
+                if dayDiff < 0:
+                    sumPlus = (lastPlus * nDay / (nDay + 1))
+                    sumMinus = (lastMinus * nDay / (nDay + 1)) + (abs(dayDiff) / (nDay + 1))
+                else:
+                    sumPlus = (lastPlus * nDay / (nDay + 1)) + (abs(dayDiff) / (nDay + 1))
+                    sumMinus = (lastMinus * nDay / (nDay + 1))
+
+            lastPlus = sumPlus
+            lastMinus = sumMinus
+
+            avgPlus = sumPlus / (nDay + 1)
+            avgMinus = sumMinus / (nDay + 1)
+
+            if avgPlus + avgMinus == 0:
+                dayRSI = None
+                A = numpy.array([None])
+            else:
+                dayRSI = avgPlus / (avgPlus + avgMinus)
+                A = numpy.array([dayRSI * 100])
+
+            B = dataArray[n]
+            B = numpy.append(B, A)
+            arrRSI.append(B)
+        else:
+            B = dataArray[n]
+            A = numpy.array([None])
+            B = numpy.append(B, A)
+            arrRSI.append(B)
+
+        # print (n, dataArray[n][0], A)
+        n = n + 1
+
+    return arrRSI
